5703619f9e69f0c066b9481daa1a52662ddf059c
André Gomes <andrelucasrngomes@gmail.com>
2025-03-14 14:57:24 -0300

    senha nova gerada

--- gerar-senha.sh
+++ gerar-senha.sh
@@ -14,12 +14,18 @@ echo "$senha" >> senhasgeradas.txt
 cd ..
 
 # Interpretar os argumentos:
+<<<<<<< HEAD
 # (Complete o código com as outras funcionalidades usando o elif)
 if [ "$1" = "-p" ] ; then
   cat /senhas/senhasgeradas.txt    
 elif [ "$1" = "-h" ] ; then
   echo -e "Bem vindo ao gerar-senha! Versão 1.0, (c) 2025, Fulano de Tal,DIMAp, UFRN
 Uso: ./gerar-senha.sh [OPÇÕES]:
+=======
+if [ "$1" = "-h" ]; then
+  echo -e "Bem vindo ao password-generator! Versão 1.0, (c) 2025, Fulano de Tal,DIMAp, UFRN
+Uso: ./password-generator.sh [OPÇÕES]:
+>>>>>>> limparSenhas
 
 Opções:
 
@@ -27,7 +33,14 @@ Opções:
 -c : Limpar as senhas do arquivo passwords.txt
 -h : Exibir esse menu
 
+<<<<<<< HEAD
 O comportamento padrão do script é gerar uma senha de 10 caracteres minúsculos."
+=======
+O comportamento padrão do script é gerar uma senha de 8 caracteres minúsculos."
+
+elif [ "$1" = "-c "]; then
+  >/senhas/senhasgeradas.txt
+>>>>>>> limparSenhas
 fi
 
 # Imprime a senha gerada
